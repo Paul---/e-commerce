8cf95edee8abcb38edca217b64f1462c4e522102
MrDean <Paul---@users.noreply.github.com>
2020-02-17 16:10:01 -0700

    add collection components and styling

--- src/components/collection-preview/CollectionPreview.component.jsx
+++ src/components/collection-preview/CollectionPreview.component.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import CollectionItem from "../collection-item/CollectionItem.component";
+import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, ...otherCollectionProps }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
-          .filter((item, indx) => indx < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
-          ))}
+          .filter((item, indx) => {
+            return indx < 4;
+          })
+          .map(({ id, ...otherItemProps }) => {
+            return <CollectionItem key={id} {...otherItemProps} />;
+          })}
       </div>
     </div>
   );
