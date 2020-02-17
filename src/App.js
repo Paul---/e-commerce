9f53347393f9ea799f1f6d4aa2084fe68dd49fd8
MrDean <Paul---@users.noreply.github.com>
2020-02-17 14:12:34 -0700

    update menu items to navigate with "withRouter" HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/h' component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/Directory.component.jsx
+++ src/components/directory/Directory.component.jsx
@@ -39,10 +39,8 @@ const Directory = () => {
   ]);
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => {
-        return (
-          <MenuItem key={id} title={title} size={size} imageUrl={imageUrl} />
-        );
+      {sections.map(({ id, ...otherSectionProps }) => {
+        return <MenuItem key={id} {...otherSectionProps} />;
       })}
     </div>
   );
