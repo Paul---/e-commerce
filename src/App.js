3d860b0f77c6e286e7d31de6d805f7ed06b7eb61
MrDean <Paul---@users.noreply.github.com>
2020-02-17 14:51:22 -0700

    add preview component and shop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage.component';
+import HomePage from './pages/homepage/HomePage.component';
+import ShopPage from './pages/shop/Shop.component'
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/h' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
