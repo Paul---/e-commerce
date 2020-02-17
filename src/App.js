f1706eeb439a85345eb2ddf1816cc9b812b39634
MrDean <Paul---@users.noreply.github.com>
2020-02-17 11:17:06 -0700

    connect sass file to homepage component and import sass files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/HomePage.component/HomePage.component';
+import HomePage from './pages/HomePage/HomePage.component';
 
 function App() {
   return (
     <div className="App">
-    <HomePage />
+      <HomePage />
     </div>
   );
 }
