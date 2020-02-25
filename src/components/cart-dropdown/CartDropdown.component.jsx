db1ed8d26fe4712ca808dab505a056468f081943
MrDean <Paul---@users.noreply.github.com>
2020-02-25 15:29:15 -0700

    add styles to cartdropdown

--- src/components/cart-dropdown/CartDropdown.component.jsx
+++ src/components/cart-dropdown/CartDropdown.component.jsx
@@ -4,16 +4,18 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/CustomButton.component';
 import CartItem from '../cart-item/CartItem.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { createStructuredSelector} from 'reselect';
+import { createStructuredSelector } from 'reselect';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
       <div className='cart-items'>
-        {cartItems.map(item => (
-          <CartItem key={item.id} item={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map(item => <CartItem key={item.id} item={item} />)
+        ) : (
+          <span className='empty-message'>Your Cart Is Empty & Lonely</span>
+        )}
       </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
