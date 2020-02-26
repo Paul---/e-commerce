ecc3c7ccf755727725a9eddd67c2eb22012e7db2
MrDean <Paul---@users.noreply.github.com>
2020-02-26 16:25:58 -0700

    normalize data by converting to object

--- src/components/collections-overview/CollectionsOverview.component.jsx
+++ src/components/collections-overview/CollectionsOverview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/CollectionPreview.component';
 
 import './collections-overview.styles.scss';
@@ -17,7 +17,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
