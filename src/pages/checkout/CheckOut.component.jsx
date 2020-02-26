56d7ff0eb5c08f3bfcba352b13501882cc3d2850
MrDean <Paul---@users.noreply.github.com>
2020-02-26 12:37:19 -0700

    fix checkout total formatting

--- src/pages/checkout/CheckOut.component.jsx
+++ src/pages/checkout/CheckOut.component.jsx
@@ -29,8 +29,12 @@ const CheckOutPage = ({ cartItems, total }) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
-      <div className="total"><span>TOTAL:${total}</span></div>
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className='total'>
+        <span>TOTAL : $ {total}</span>
+      </div>
     </div>
   );
 };
