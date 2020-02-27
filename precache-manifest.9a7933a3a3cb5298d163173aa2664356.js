b4a7bc176d58d7fac6b53e1785c5486837171827
MrDean <Paul---@users.noreply.github.com>
2020-02-27 10:09:46 -0700

    Updates

---
+++ precache-manifest.9a7933a3a3cb5298d163173aa2664356.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d6d6182c553ffbd7991f015883951d",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "6fc9d00eadbe115f691d",
+    "url": "/e-commerce/static/css/main.b38b294f.chunk.css"
+  },
+  {
+    "revision": "35df638776f45111e343",
+    "url": "/e-commerce/static/js/2.833c1a0c.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/e-commerce/static/js/2.833c1a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc9d00eadbe115f691d",
+    "url": "/e-commerce/static/js/main.c8203b73.chunk.js"
+  },
+  {
+    "revision": "fb5fdb8c70dfc626176f",
+    "url": "/e-commerce/static/js/runtime-main.f6ac73e2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/e-commerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
