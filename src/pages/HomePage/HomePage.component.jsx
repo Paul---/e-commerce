c7a3a893f4dd8e8e7e9f51f44a8ad0561983102b
MrDean <Paul---@users.noreply.github.com>
2020-02-28 11:08:51 -0700

    fix header component alignment

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './homepage.styles.scss';
 import Directory from '../../components/directory/Directory.component';
 
 import { HomePageContainer } from './Homepage.styles';
