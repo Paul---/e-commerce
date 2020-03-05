56ca5b8895ee1c2cb5ff4c9869854957fca52b51
MrPaul <Paul---@users.noreply.github.com>
2020-03-05 11:56:52 -0700

    linkedin code widget

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -6,6 +6,8 @@ const HomePage = () => {
   return (
     <section className='homepage'>
       <Directory />
+      <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="paul-pilcher"><a class="LI-simple-link" href='https://www.linkedin.com/in/paul-pilcher?trk=profile-badge'>Paul Pilcher</a></div>
+      <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
     </section>
   );
 };
