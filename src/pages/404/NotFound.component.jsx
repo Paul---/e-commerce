e062e027c92bb4c1fc8253eb20e83b9663886a7a
MrDean <Paul---@users.noreply.github.com>
2020-03-12 12:33:57 -0600

    add 404 and thankyou pages with basic styles

--- src/pages/404/NotFound.component.jsx
+++ src/pages/404/NotFound.component.jsx
@@ -4,7 +4,9 @@ import './NotFound.styles.scss';
 const NotFound = () => {
   return (
     <div>
-      <h1 id='notFoundMessage'>This page was not found. Please check your URL.</h1>
+      <h1 className='notFoundMessage'>
+        This page was not found. Please check your URL.
+      </h1>
     </div>
   );
 };
--- src/pages/thank-you/ThankYou.component.jsx
+++ src/pages/thank-you/ThankYou.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './ThankYou.styles.scss';
 
 const ThankYou = () => {
   return (
-    <div className='container'>
-      <h1 className='thanks'>
+    <div>
+      <h1 className='thankYou'>
         Thank you for your business. Your order is on its way!
       </h1>
     </div>
