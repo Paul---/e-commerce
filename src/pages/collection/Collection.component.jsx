d7864b343aab313b0031754e45670833ccac5876
MrDean <Paul---@users.noreply.github.com>
2020-03-23 15:37:00 -0600

    add suspense/lazyl loading basic functionality to Collection Component

--- src/pages/collection/Collection.component.jsx
+++ src/pages/collection/Collection.component.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selectors';
-import CollectionItem from '../../components/collection-item/CollectionItem.component';
+// import CollectionItem from '../../components/collection-item/CollectionItem.component';
 import './collection.styles.scss';
+
+const CollectionItem = React.lazy(() =>
+  import('../../components/collection-item/CollectionItem.component')
+);
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
+  debugger;
 
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className='items'>
+          {items.map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 };
