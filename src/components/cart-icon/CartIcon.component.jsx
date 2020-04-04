4a0e91281cf59d1a2e527e27ef4fe765f0458060
MrDean <Paul---@users.noreply.github.com>
2020-04-04 13:34:44 -0600

    remove console.log and useless selector

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+
   const dispatch = useDispatch();
   const itemCount = useSelector((state) => state.cart.cartItems.length);
-  const redux = useSelector((state) => state);
-  console.log('rendered', redux);
+
   return (
     <div className='cart-icon' onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className='shopping-icon' />
