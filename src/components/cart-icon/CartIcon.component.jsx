ae7bef1e680def49d1e5056c62e66145f3f4c5a6
MrDean <Paul---@users.noreply.github.com>
2020-04-24 17:04:09 -0600

    update packages and add logger to dev dependencies

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -7,7 +7,6 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-
   const dispatch = useDispatch();
   const itemCount = useSelector((state) => state.cart.cartItems.length);
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import { persistStore } from 'redux-persist';
-//import logger from 'redux-logger';
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 // for future middleware updates
-const middlewares = [];
+const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
